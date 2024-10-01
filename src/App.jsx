ef8a12f86211861f299e01283befc31c829d47d2
Oliver Sitzmann <oliversitzmannr@gmail.com>
2024-09-30 20:27:07 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="top-nav"></div>
+      <div className="main">
+          <div className="sub-content"></div>
+          <div className="sub-content"></div>
+          <div className="sub-content"></div>
+          <div className="advertisement"></div>
+      </div>
+      <div className="side-nav"></div>
+    </div>
+  )
+}
+
+export default App
